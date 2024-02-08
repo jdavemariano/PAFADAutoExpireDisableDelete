c64992f6d76258c4fc7ee025636f48340bf93887
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2024-02-08 22:44:29 +0800

    Update updateexpirybyemail.py

--- updateexpirybyemail.py
+++ updateexpirybyemail.py
@@ -28,7 +28,7 @@ ssm_client = boto3.client('ssm', region_name="us-east-1")
 
 ssm_create_response = ssm_client.create_document(Content = ssm_json, Name = ssm_doc_name, DocumentType = 'Command', DocumentFormat = 'JSON', TargetType =  "/AWS::EC2::Instance")
 
-ssm_run_response = ssm_client.send_command(InstanceIds = [target_domain], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120,  Parameters={'Email':[target_emailid],'Cutoffdate':[cutoffdate]})
+ssm_run_response = ssm_client.send_command(InstanceIds = [target_domain], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120,  Parameters={'EmailID':[target_emailid],'Cutoffdate':[cutoffdate]})
 print(f'{ssm_run_response}\n')
 cmd_id = ssm_run_response['Command']['CommandId']
 
@@ -39,7 +39,7 @@ while ssm_status_response['StatusDetails'] == 'InProgress':
 	ssm_status_response = ssm_client.get_command_invocation(CommandId=cmd_id, InstanceId=target_domain)
 
 if ssm_status_response['StatusDetails'] == 'Success':
-	print(f'User {target_username} account expiration update to {cutoffdate} has been triggered on {target_domain}\n')
+	print(f'User {target_emailid} account expiration update to {cutoffdate} has been triggered on {target_domain}\n')
 
 cmd_output = ssm_status_response.get('StandardOutputContent','')
 print(f'{cmd_output}\n')
